f4c5bf0a0617c5a0177ee1f7e9b5539394290655
Kevin <kevinfiorentino.1294@gmail.com>
2022-12-28 14:23:09 -0300

    Add token mint instruction and dependencies

--- programs/solana-movies-tokens/src/lib.rs
+++ programs/solana-movies-tokens/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke_signed;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+use mpl_token_metadata::instruction::create_metadata_accounts_v2;
 
 declare_id!("7K761aFhTwzkqyJabPV7TahWegRxLFNtbLWghF4kzje3");
 
@@ -46,6 +50,53 @@ pub mod solana_movies_tokens {
     pub fn close(_ctx: Context<Close>) -> Result<()> {
         Ok(())
     }
+
+    pub fn create_reward_mint(
+        ctx: Context<CreateTokenReward>,
+        uri: String,
+        name: String,
+        symbol: String,
+    ) -> Result<()> {
+        msg!("Create Reward Token");
+
+        let seeds = &["mint".as_bytes(), &[*ctx.bumps.get("reward_mint").unwrap()]];
+
+        let signer = [&seeds[..]];
+
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.reward_mint.to_account_info(),
+            ctx.accounts.user.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+
+        invoke_signed(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.reward_mint.key(),
+                ctx.accounts.reward_mint.key(),
+                ctx.accounts.user.key(),
+                ctx.accounts.user.key(),
+                name,
+                symbol,
+                uri,
+                None,
+                0,
+                true,
+                true,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+            &signer,
+        )?;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -82,6 +133,32 @@ pub struct UpdateMovieReview<'info> {
 }
 
 #[derive(Accounts)]
+pub struct CreateTokenReward<'info> {
+    #[account(
+        init,
+        seeds = ["mint".as_bytes().as_ref()],
+        bump,
+        payer = user,
+        mint::decimals = 6,
+        mint::authority = reward_mint,
+
+    )]
+    pub reward_mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,
+
+    /// CHECK:
+    #[account(mut)]
+    pub metadata: AccountInfo<'info>,
+    /// CHECK:
+    pub token_metadata_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
 pub struct Close<'info> {
     #[account(mut, close = reviewer, has_one = reviewer)]
     movie_review: Account<'info, MovieAccountState>,
@@ -95,4 +172,10 @@ pub struct MovieAccountState {
     pub rating: u8,          // 1
     pub title: String,       // 4 + len()
     pub description: String, // 4 + len()
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Rating greater than 5 or less than 1")]
+    InvalidRating,
 }
